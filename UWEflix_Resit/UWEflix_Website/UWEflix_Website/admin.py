e3c453f3cf799983bb6159758dd81877a6e3ddd4
AidenDymott <97362961+AidenDymott@users.noreply.github.com>
2023-06-13 16:58:24 +0100

    Can now view/cancel Bookings

---
+++ UWEflix_Resit/UWEflix_Website/UWEflix_Website/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile, Movie, Showing, Booking
+
+# Unregister Groups for easier viewing
+admin.site.unregister(Group)
+
+# Combine profile and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+# Extends User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = [ "username" ]
+    inlines = [ProfileInline]
+    
+# Unregister User Temp
+admin.site.unregister(User)
+
+# Register Models
+admin.site.register(User, UserAdmin)
+admin.site.register(Movie)
+admin.site.register(Showing)
+admin.site.register(Booking)
+
+# Combine profile and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
---
+++ UWEflix_Resit/UWEflix_Website/UWEflix_Website/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User, Group
+from django.db.models.signals import post_save
+
+# Create user profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # One user has one profile
+    
+    def __str__(self):
+        return self.user.username
+    
+# Auto create user profile on sign up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user= instance )
+        user_profile.save()
+        
+post_save.connect(create_profile, sender = User)
+
+# Movie Database
+class Movie(models.Model):
+    movie_name = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 500)
+    rating= models.CharField(max_length = 3)
+    
+    def __str__(self):
+        return self.movie_name
+    
+# Showings Database
+
+class Showing(models.Model):
+    movie = models.ForeignKey(Movie, blank=True, null=True, on_delete=models.CASCADE)
+    date_showing = models.DateField('Movie Showing Date')
+    time_showing = models.TimeField('Movie Showing Time')
+    seats = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.movie) + ' ' + str(self.date_showing) + ' ' + str(self.time_showing)
+
+# Booking DB
+
+class Booking(models.Model):
+    booked_movie = models.ForeignKey(Movie, blank=True, null=True, on_delete=models.CASCADE, related_name='bmovie')
+    viewing_date = models.ForeignKey(Showing, blank=True, null=True, on_delete=models.CASCADE ,related_name='bdate')
+    viewing_time = models.ForeignKey(Showing, blank=True, null=True, on_delete=models.CASCADE ,related_name='btime')
+    booked_seats = models.ForeignKey(Showing, blank=True, null=True, on_delete=models.CASCADE, related_name='bseats')
+    
+    def __str__(self):
+        return str(self.booked_movie) + ' ' + str(self.viewing_date) + ' ' + str(self.booked_seats)
+    
+    
