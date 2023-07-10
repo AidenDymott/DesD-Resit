756080f15e7dc9bbecc69251e1569dd32565739c
AidenDymott <97362961+AidenDymott@users.noreply.github.com>
2023-07-10 21:39:26 +0100

    Club Styling start + Balance payments

---
+++ UWEflix_Resit/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import (Profile, Movie, Showing, Booking, Screen, Club, 
+                     Ticket)
+
+# Combine profile and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+# Extends User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = [ "username" ]
+    inlines = [ProfileInline]
+    
+# Unregister User Temp
+admin.site.unregister(User)
+
+# Register Models
+admin.site.register(User, UserAdmin)
+admin.site.register(Movie)
+admin.site.register(Showing)
+admin.site.register(Booking)
+admin.site.register(Screen)
+admin.site.register(Club)
+admin.site.register(Ticket)
+
+# Combine profile and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
